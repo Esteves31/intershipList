cd149da9e7c535116c79026b7244ec957e0d9dcf
Matheus <matheus.resteves@gmail.com>
2024-02-19 19:11:07 -0300

    feat: add a simple .h

---
+++ intership.h
@@ -0,0 +1,16 @@
+#ifndef INTERSHIP_H
+#define INTERSHIP_H
+
+
+typedef struct Intership intership;
+
+struct Intership {
+
+};
+
+void addIntership();
+void removeIntership();
+void listInterships();
+
+
+#endif
