2b587ac0dd2167f25a350bc51ecbebf1a7c05c05
Matheus <matheus.resteves@gmail.com>
2024-02-20 14:31:59 -0300

    feat: add a struct intership

--- intership.h
+++ intership.h
@@ -1,16 +1,20 @@
 #ifndef INTERSHIP_H
 #define INTERSHIP_H
 
+#include <stdbool.h>
 
 typedef struct Intership intership;
 
 struct Intership {
-
+    char companyName[20];
+    char occupationArea[30];
+    char type;
+    int index;
+    bool isActive;
 };
 
 void addIntership();
 void removeIntership();
 void listInterships();
-
 
 #endif
